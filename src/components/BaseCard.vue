019b7d675f33afab294c2f20d310cf21452b60f7
Chandan Kumar <chandanchaudharyrules@gmail.com>
2018-07-26 02:55:11 +0530

    Make ux method card a generic base card

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -2,8 +2,8 @@
   <v-card>
     <v-card-title primary-title>
       <base-headline
-        v-bind:title="method.title"
-        v-bind:description="method.description"
+        v-bind:title="card.title"
+        v-bind:description="card.description"
         class="pb-5"
       >
       </base-headline>
@@ -11,8 +11,13 @@
     <v-spacer></v-spacer>
     <v-card-actions>
       <v-spacer></v-spacer>
-      <v-btn flat small v-bind:ripple="false">
-        Use this <v-icon right>arrow_right_alt</v-icon>
+      <v-btn flat small
+        v-bind:ripple="false"
+        v-for="action in actions"
+        v-bind:key="action.label"
+        v-on:click="action.callback(card)"
+      >
+        {{ action.label }} <v-icon right>{{ action.icon }}</v-icon>
       </v-btn>
       <v-spacer></v-spacer>
     </v-card-actions>
@@ -23,9 +28,14 @@
 export default {
   name: 'UXMethodCard',
   props: {
-    method: {
+    card: {
       type: Object,
       required: true,
+    },
+    actions: {
+      type: Array,
+      required: false,
+      default: () => [],
     },
   },
 };
--- src/components/UXMethods.vue
+++ src/components/UXMethods.vue
@@ -10,7 +10,7 @@
     <v-container fluid align-center grid-list-lg pa-0>
       <v-layout row wrap>
         <v-flex xl2 lg3 sm4 xs12 v-for="item in items" v-bind:key="item.id">
-          <ux-method-card v-bind:method="item"></ux-method-card>
+          <base-card v-bind:actions="actions" v-bind:card="item"></base-card>
         </v-flex>
       </v-layout>
     </v-container>
@@ -18,17 +18,34 @@
 </template>
 
 <script>
-import UXMethodCard from '@/components/UXMethodCard.vue';
 
 export default {
   name: 'UXMethods',
   components: {
-    'ux-method-card': UXMethodCard,
   },
   props: {
     items: {
       type: Array,
       required: true,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      actions: [
+        {
+          label: 'Use this',
+          icon: 'arrow_right_alt',
+          callback: (method) => {
+            this.use(method);
+          },
+        },
+      ],
+    };
+  },
+  methods: {
+    use(method) {
+      console.log(method);
     },
   },
 };
